ad21a3d78ae3d2d7877cfe96759f63a27f22dfa0
Pooja <poojatobriya@gmail.com>
2024-08-09 11:38:20 +0530

    implementing Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand">Navbar</a>
+    <form className="d-flex" role="search">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  )
+}
+
+export default navbar
